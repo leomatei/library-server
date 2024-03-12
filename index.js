b18596c8fdbec353021fe96d1f9d792a887b343f
Matei Leonard <leonard.matei2011@yahoo.com>
2024-03-12 15:54:28 +0200

    delete unused code

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// server.js
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
 const cors = require('cors')
 
-// Define your GraphQL schema
 const schema = buildSchema(`
   type Book{
     id: ID
@@ -72,27 +70,22 @@ const root = {
   },
 }
 
-// Create an Express server
 const app = express()
-
 app.use(cors())
-
 app.use(express.json())
 
-// Set up the /graphql endpoint with the GraphQL middleware
 app.use(
   '/api',
   graphqlHTTP({
     schema: schema,
     rootValue: root,
-    graphiql: true, // Enable GraphiQL for easy testing
+    graphiql: true,
   })
 )
 app.use('/', (req, res) => {
-  res.send('server is runing.')
+  res.send('Server is running!')
 })
 
-// Start the server on port 4000
 const PORT = 4000
 app.listen(PORT, () => {
   console.log(`GraphQL server running at http://localhost:${PORT}/api`)
