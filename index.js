036ff30912b4678ecd4062b2798e31f4958980d3
Matei Leonard <leonard.matei2011@yahoo.com>
2024-03-13 22:39:40 +0200

    add posgresql vercel db on project

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
 const cors = require('cors')
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: `${process.env.REACT_APP_POSTGRES_URL}?sslmode=require`,
+})
+
+pool.connect((err) => {
+  if (err) {
+    throw err
+  }
+  console.log('Connected to PostgreSQL!')
+})
 
 const schema = buildSchema(`
   type Book{
@@ -26,47 +38,61 @@ const schema = buildSchema(`
   }
 `)
 
-let books = [
-  {
-    id: 1,
-    title: 'book1',
-    author: 'author1',
-    description:
-      'desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1',
-  },
-  { id: 2, title: 'book2', author: 'author2', description: 'desc2' },
-  { id: 3, title: 'book3', author: 'author3', description: 'desc3' },
-]
-
 const root = {
-  getBooks: () => books,
-  getBook: ({ id }) => {
-    const index = books.findIndex((book) => book.id == id)
-    if (index !== -1) {
-      return books[index]
+  getBooks: async () => {
+    const queryText = 'SELECT * FROM book'
+    const client = await pool.connect()
+    try {
+      const { rows } = await client.query(queryText)
+      return rows
+    } finally {
+      client.release()
     }
-    return null
   },
-  createBook: ({ input }) => {
-    const newBook = { id: books.length + 1, ...input }
-    books.push(newBook)
-    return newBook
-  },
-  updateBook: ({ id, input }) => {
-    const index = books.findIndex((book) => book.id == id)
-    if (index !== -1) {
-      books[index] = { ...books[index], ...input }
-      return books[index]
+  getBook: async ({ id }) => {
+    const queryText = 'SELECT * FROM book WHERE id = $1'
+    const values = [id]
+    const client = await pool.connect()
+    try {
+      const { rows } = await client.query(queryText, values)
+      return rows[0]
+    } finally {
+      client.release()
     }
-    return null
   },
-  deleteBook: ({ id }) => {
-    const index = books.findIndex((book) => book.id == id)
-    if (index !== -1) {
-      books.splice(index, 1)
-      return `Book with ID ${id} deleted successfully.`
+  createBook: async function createBook({ input }) {
+    const queryText =
+      'INSERT INTO book (title, author, description) VALUES ($1, $2, $3) RETURNING *'
+    const values = [input.title, input.author, input.description]
+    const client = await pool.connect()
+    try {
+      const { rows } = await client.query(queryText, values)
+      return rows[0]
+    } finally {
+      client.release()
     }
-    return `Book with ID ${id} not found.`
+  },
+  updateBook: async ({ id, input }) => {
+    const queryText =
+      'UPDATE book SET title = $1, author = $2, description = $3 WHERE id = $4 RETURNING *'
+    const values = [input.title, input.author, input.description, id]
+    const client = await pool.connect()
+    try {
+      const { rows } = await client.query(queryText, values)
+      return rows[0]
+    } finally {
+      client.release()
+    }
+  },
+  deleteBook: async ({ id }) => {
+    const queryText = 'DELETE FROM book WHERE id = $1'
+    const values = [id]
+    const client = await pool.connect()
+    try {
+      await client.query(queryText, values)
+    } finally {
+      client.release()
+    }
   },
 }
 
